3c687f666c61fdaf0324d6dabceff0a0e4948424
mlahane <mlahane@mlahane.pnq.csb>
2019-12-24 20:39:45 +0530

    Simple program

---
+++ class_methods_call.py
@@ -0,0 +1,20 @@
+# class Resto(object):
+# class Resto():
+class Resto:	
+	
+	def fun(self):
+		print("hello")
+
+
+	def fun2(y):
+		print("fun2")
+Resto().fun()
+
+
+
+#x=Resto()
+
+#x.fun()
+#x.fun2()
+	
+	
---
+++ const.py
@@ -0,0 +1,20 @@
+class Teest():
+
+	y=6
+		
+	
+	def __init__(self):
+		self.a=5
+		
+
+	def foo(self):
+		print(self.a)
+		print(self.y)		
+#	def __init__(self):
+#		print("say hi")
+
+
+#	def __init__(self):
+#		print("hhhhh")
+x=Teest()
+x.foo()
